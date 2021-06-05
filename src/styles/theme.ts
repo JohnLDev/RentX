9f92d565fd05078f9debddc106ee7ca28692bd9b
JohnLDev <john.lenon.dev@gmail.com>
2021-06-05 16:12:31 -0300

    começo do projeto

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+export default {
+  colors: {
+    header:'#1b1b1f',
+
+    background_primary: '#f4f5f6',
+    background_secondary: '#fff',
+
+    text: '#7a7a80',
+    text_detail:'#aeaeb3',
+    title: '#47474d',
+
+    line: '#ebebf0',
+
+    main: '#dc1637',
+    main_light: '#fdedef',
+    success: '#03b252',
+
+    shape: '#e1e1e8',
+    shape_dark: '#29292e',
+
+  },
+
+  fonts: {
+    secondary_regular: 'Archivo_regularRegular',
+    secondary_medium: 'Archivo_500Medium',
+    secondary_semiBold:'Archivo_600SemiBold',
+    primary_regular: 'Inter_400Regular',
+    primary_medium: 'Inter_500Medium',
+  },
+}
